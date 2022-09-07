5758d6cf867b434b426dea972a724b369f78399e
F. Souzza <filipesouzza.adm@gmail.com>
2022-09-07 17:33:44 -0300

    alteracoes sass

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ import { KeyDrums } from 'data/KeyDrums';
 
 export const App = () => {
   const clickKeyBoard = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.textContent)
+    console.log(e.currentTarget.ELEMENT_NODE);
+    
   }
   
-  
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.value);
     const audio = new Audio(`sounds/key${e.currentTarget.value}.mp3`);
     audio.play();
   }
